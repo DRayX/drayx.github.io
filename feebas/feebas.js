5e2a883626bd72f24e923e21cf451ddfd433d453
DRayX <7531689+DRayX@users.noreply.github.com>
2021-11-26 15:06:26 -0800

    Basic files for feebas calculator

---
+++ feebas/feebas.js
@@ -0,0 +1,52 @@
+import {TILE_DATA} from './tiles.js';
+import {loadImage} from './util.js';
+
+const TILE_SIZE = 16;
+
+function draw(canvas, map, lotto) {
+  const ctx = canvas.\.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(map, 0, 0);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'black';
+  ctx.beginPath();
+  for (var x = 0; x <= canvas.width; x += TILE_SIZE) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+  }
+  for (var y = 0; y <= canvas.height; y += TILE_SIZE) {
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+  }
+  ctx.stroke();
+  const seed_pos = (lotto * 0xeb65 + 0xa683) & 0xffff;
+  const seed_neg = 0x10000 - seed;
+  const location_low_pos = TILE_DATA[0x18f + (seed_pos & 0xff) % 0x84];
+  const location_low_neg = TILE_DATA[0x18f + (seed_neg & 0xff) % 0x84];
+  const location_high_pos = TILE_DATA[0x10a * 2 + (seed_pos >> 8) % 0x84];
+  const location_high_neg = TILE_DATA[0x10a * 2 + (seed_neg >> 8) % 0x84];
+  ctx.fillStyle = 'green';
+  for (const [color, locations] of Object.entries({
+      'green': [location_low_pos, location_high_pos],
+      'red': [location_low_neg, location_high_neg],
+  })){
+    ctx.fillStyle = color;
+    for (loc of locations) {
+      ctx.fillRect(
+          0.5 + (loc[0] - TILE_DATA[0][0]) * TILE_SIZE,
+          0.5 + (loc[1] - TILE_DATA[0][1]) * TILE_SIZE,
+          TILE_SIZE - 1,
+          TILE_SIZE - 1);
+    }
+  }
+}
+
+(async function() {
+  const map = await loadImage('./map.png');
+  const canvas = document.getElementById('canvas');
+  [canvas.width, canvas.height] = [map.width, map.height];
+  const ctx = canvas.getContext('2d');
+  const input = document.getElementById('lottery');
+  input.addEventListener('input', () => draw(canvas, map, input.value));
+  draw(canvas, map, input.value);
+})();
---
+++ feebas/util.js
@@ -0,0 +1,8 @@
+export function function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => resolve(img));
+    img.addEventListener('error', reject);
+    img.src = src;
+  });
+}
